cc306d4b1c27ad77076c3de25c18155e7c8fb02a
patmarz <patrykmarz24@gmail.com>
2025-09-17 18:14:36 -0500

    Commit v1.1.6

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,24 @@
-const CACHE = "uiuc-course-compass-v1";
+// v1.1.6 cache bump
+const CACHE = 'uicc-v1.1.6';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest"
+  './',
+  './index.html',
+  './styles.css',
+  './app.js?v=116',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))));
 });
-
-self.addEventListener("fetch", (e) => {
-  const { request } = e;
-  if (request.method !== "GET") return;
-
-  e.respondWith(
-    caches.match(request).then(cached => {
-      const fetchPromise = fetch(request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(cache => cache.put(request, copy));
-        return res;
-      }).catch(() => cached);
-      return cached || fetchPromise;
-    })
-  );
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('/app.js')) {
+    e.respondWith(fetch(e.request).then(r => (caches.open(CACHE).then(c=>c.put(e.request, r.clone())), r)).catch(()=>caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
 });
