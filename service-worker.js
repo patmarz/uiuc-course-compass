cb02324aaddddb8a8d3b33a51f14392a8f1798c5
patmarz <patrykmarz24@gmail.com>
2025-11-10 15:52:04 -0600

    Commit v1.1.9

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='uicc-v1.2.3';
-const ASSETS=['./','./index.html','./styles.css','./app.js?v=123','./data/testimonials.json?v=123','./data/tags.json?v=123'];
+const CACHE='uicc-v1.1.9';
+const ASSETS=['./','./index.html','./styles.css','./app.js?v=119','./data/gpa.json','./data/testimonials.json','./data/tags.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
